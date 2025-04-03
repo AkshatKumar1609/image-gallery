a1bf2b2b3b17722ed94dd5139fe6182c65fede3e
AkshatKumar1609 <akshatk.ug23.cs@nitp.ac.in>
2025-04-03 23:54:29 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const API_URL = 'https://api.unsplash.com/search/photos';
+  const IMAGES_PER_PAGE = 30;
+  const API_KEY = 'j6s69QIDBlmnr56riJN84ucjl1GNDOfv4GHurd8bJk0';
+
+  let [search,setSearch] = useState('')
+  let [imageData,setImageData] = useState([])
+  let [page,setPage] = useState(1)
+  let [totalPages,setTotalPages] = useState(0)
+
+  function fetchData(event){
+    fetch( `${API_URL}?query=${search}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${API_KEY}`)
+    .then((res)=>res.json())
+    .then((finalRes)=>{
+      setImageData(finalRes.results)
+      console.log(finalRes)
+      setTotalPages(finalRes.total_pages)
+    })
+  }
+
+  function functionToFetch(event){
+    event.preventDefault();
+    fetchData()
+  }
+
+  useEffect(()=>{
+    if (search) {
+      fetchData();
+    }
+  },[page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Image Gallery</h1>
+      <form onSubmit={(event)=>functionToFetch(event)}>
+        <input type='text' value={search} onChange={(event)=>setSearch(event.target.value)}/>
+        <button type='submit'>Search</button>
+      </form>
+      {
+        (imageData)?
+        <div>
+        {
+          imageData.map((v,i)=>{
+            return(
+              <img src={v.urls.full} style={{height:`100px`}} alt={v.alt_description} key={i}/>
+            )
+          })
+        }
+      </div>
+      :
+      ''
+      }
+      <div>
+        {page > 1 && <button onClick={() => setPage(page-1)}>p</button>}
+        {page < totalPages && <button onClick={() => setPage(page+1)}>n</button>}
+      </div>
+    </>
   );
 }
 
